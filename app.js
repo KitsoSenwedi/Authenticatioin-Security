14393c0003bce78eac3d0cb8b04017985a94cf36
kitsosenwedi <boyboykitso@gmail.com>
2023-12-10 15:41:32 +0200

    mongoose failed to install

--- app.js
+++ app.js
@@ -2,12 +2,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
 
 app.get("/", function(req, res){
     res.render("home");
@@ -21,7 +29,20 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
 
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("secrets");
+        }
+    })
+});
 
 
 
