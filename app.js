798e2ea202d9186627784e3e6f0da45354ba4d78
kitsosenwedi <boyboykitso@gmail.com>
2023-12-16 14:26:37 +0200

    mongoose debugging

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema =  ({
     email: String,
     password: String
-};
+});
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
     res.render("home");
